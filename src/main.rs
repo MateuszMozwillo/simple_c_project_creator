faf966476cf195dd5285a08a4aae80aca14823a6
MateuszMozwillo <mateusz.mozwillo@gmail.com>
2024-03-09 23:48:19 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,53 @@
-use std::{env::{self, current_exe}, process::Command};
+use std::{env::{self}, process::{exit, Command}};
+
+const CONFIG_DIR: &str = "/Users/mai/Documents/My_bin/config";
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("too many arguments");
+        exit(1);
+    } if args.len() < 2 {
+        println!("too few arguments");
+        exit(1);
+    } if args[1] == "-h" || args[1] == "--h" || args[1] == "-help" || args[1] == "--help" {
+        println!("to create a c/c++ project provide project name as argument");
+        exit(1);
+    } if args[1].starts_with("-") || args[1].starts_with("--") {
+        println!("argument not supprted");
+        exit(1);
+    }
+
     let project_name: String = args[1].clone();
 
     let command_out_pwd = Command::new("pwd")
         .output().expect("pwd fail");
-
+    if command_out_pwd.stderr.len() > 0 {
+        println!("{}", String::from_utf8(command_out_pwd.stderr).unwrap());
+        exit(1);
+    }
     let pwd_out_as_char = 
         String::from_utf8(command_out_pwd.stdout).expect("char* conversion fail");
-    
     let pwd_trimmed = pwd_out_as_char.trim();
 
     let project_dir = format!("{}/{}", &pwd_trimmed, &project_name);
-    println!("{}", &project_dir);
+    println!("project created in: {}", &project_dir);
     
-    Command::new("mkdir").arg(&project_dir)
+    let mkdir_cmnd_out = Command::new("mkdir").arg(&project_dir)
         .output().expect("mkdir err");
-    let exe_dir = current_exe().expect("cant get dir of exe")
-        .into_os_string().into_string().unwrap();
-    println!("{}, suffix: {}", &exe_dir, format!("/{}", &project_name));
-    let exe_dir_stripped = exe_dir.strip_suffix("/c-project-creator").expect("couldnt remove suffix");
-    println!("{}", &exe_dir_stripped);
-    Command::new("cp").arg("-r").arg(
-            format!("{}/project_template/", &exe_dir_stripped)
-        )
-        .arg(&project_dir).output().expect("copy err");    
+    if mkdir_cmnd_out.stderr.len() > 0 {
+        println!("{}", String::from_utf8(mkdir_cmnd_out.stderr).unwrap());
+        exit(1);
+    }
+
+    let copy_cmnd_out = Command::new("cp").arg("-r").arg(
+            format!("{}/c-project-creator/project_template/", CONFIG_DIR)
+        ).arg(&project_dir).output().expect("copy err");
+    if copy_cmnd_out.stderr.len() > 0 {
+        println!("{}", String::from_utf8(copy_cmnd_out.stderr).unwrap());
+        exit(1);
+    }
+
 }

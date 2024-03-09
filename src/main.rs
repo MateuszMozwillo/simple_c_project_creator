d8c202784b4b7972d710e306c9ab2acacba187e0
MateuszMozwillo <mateusz.mozwillo@gmail.com>
2024-03-09 22:55:03 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fmt::format, process::Command};
+use std::{env::{self, current_exe}, process::Command};
 
 fn main() {
 
@@ -10,14 +10,21 @@ fn main() {
 
     let pwd_out_as_char = 
         String::from_utf8(command_out_pwd.stdout).expect("char* conversion fail");
+    
     let pwd_trimmed = pwd_out_as_char.trim();
 
-    let project_dir = format!("{}/{}", pwd_trimmed, &project_name);
+    let project_dir = format!("{}/{}", &pwd_trimmed, &project_name);
     println!("{}", &project_dir);
+    
     Command::new("mkdir").arg(&project_dir)
         .output().expect("mkdir err");
-
-    Command::new("cp").arg("-r").arg("./project_template/")
-        .arg(&project_dir).output().expect("copy err");
-    
+    let exe_dir = current_exe().expect("cant get dir of exe")
+        .into_os_string().into_string().unwrap();
+    println!("{}, suffix: {}", &exe_dir, format!("/{}", &project_name));
+    let exe_dir_stripped = exe_dir.strip_suffix("/c-project-creator").expect("couldnt remove suffix");
+    println!("{}", &exe_dir_stripped);
+    Command::new("cp").arg("-r").arg(
+            format!("{}/project_template/", &exe_dir_stripped)
+        )
+        .arg(&project_dir).output().expect("copy err");    
 }

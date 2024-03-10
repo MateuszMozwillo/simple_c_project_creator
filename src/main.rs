4c3ccc229f1bdf7bbe151da5300de68d9a78ee27
MateuszMozwillo <mateusz.mozwillo@gmail.com>
2024-03-10 11:11:51 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::{self}, fs, io::Write, process::exit};
+use std::io::Write;
 
 fn main() {
 
@@ -6,32 +6,32 @@ fn main() {
     let gitignore_template = include_bytes!("./project_template/.gitignore");
     let makefile_template = include_bytes!("./project_template/Makefile");
 
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> =std::env::args().collect();
 
     if args.len() > 2 {
         println!("too many arguments");
-        exit(1);
+        std::process::exit(1);
     } if args.len() < 2 {
         println!("too few arguments");
-        exit(1);
+        std::process::exit(1);
     } if args[1] == "-h" || args[1] == "--h" || args[1] == "-help" || args[1] == "--help" {
         println!("to create a c/c++ project provide project name as argument");
-        exit(1);
+        std::process::exit(1);
     } if args[1].starts_with("-") || args[1].starts_with("--") {
         println!("argument not supprted");
-        exit(1);
+        std::process::exit(1);
     }
 
     let project_name: String = args[1].clone();
 
-    fs::create_dir_all(format!("./{}/src", &project_name)).unwrap();
+    std::fs::create_dir_all(format!("./{}/src", &project_name)).unwrap();
 
-    let mut makefile_file = fs::File::create(format!("./{}/Makefile", &project_name)).unwrap();
+    let mut makefile_file = std::fs::File::create(format!("./{}/Makefile", &project_name)).unwrap();
     makefile_file.write_all(makefile_template).unwrap();
 
-    let mut gitignore_file = fs::File::create(format!("./{}/.gitignore", &project_name)).unwrap();
+    let mut gitignore_file = std::fs::File::create(format!("./{}/.gitignore", &project_name)).unwrap();
     gitignore_file.write_all(gitignore_template).unwrap();
 
-    let mut main_file = fs::File::create(format!("./{}/src/main.cpp", &project_name)).unwrap();
+    let mut main_file = std::fs::File::create(format!("./{}/src/main.cpp", &project_name)).unwrap();
     main_file.write_all(main_cpp_template).unwrap();
 } 
